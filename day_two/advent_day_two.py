ba54b3f7798bfe4f8fa29b3c0b8dcd29e068dfe3
Calista Kim <ckim@101608-MAC.local>
2018-01-10 08:56:08 -0500

    day two setup

---
+++ day_two/advent_day_two.py
@@ -0,0 +1,19 @@
+'''
+DAY TWO
+
+checkSum is a function that determines the difference between the
+largest value and the smallest value for each row of a matrix, then
+sums all of the differences.
+
+'''
+
+import numpy
+
+puzzle = numpy.matrix([[1, 2], [3, 4]])
+
+def checkSum(matrix):
+
+    print matrix
+
+
+checkSum(puzzle)
